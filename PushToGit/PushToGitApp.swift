59e31e8ec673e3bcb516214d6b9a8ae2acc8b27c
Oloyede2001 <Jesutomisinoloyede@gmail.com>
2024-03-24 14:42:47 -0400

    Initial Commit

---
+++ PushToGit/PushToGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushToGitApp.swift
+//  PushToGit
+//
+//  Created by Jesutomisin Oloyede on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PushToGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
